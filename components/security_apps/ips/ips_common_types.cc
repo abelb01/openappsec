311750e2a3f8a7ac355d766deff677deba493a36
abelb01 <44084096+abelb01@users.noreply.github.com>
2023-11-14 22:14:06 +0000

    initial commit

--- components/security_apps/ips/ips_common_types.cc
+++
@@ -1,4 +0,0 @@
-#include "ips_common_types.h"
-
-const Buffer IPSCommonTypes::requests_header_for_log =
-    Buffer("agent-found-Requests-header-for-log", 35, Buffer::MemoryType::STATIC);
--- components/security_apps/orchestration/update_communication/fog_helper_open_source.cc
+++
@@ -1,9 +0,0 @@
-#include "fog_authenticator.h"
-
-#include <string>
-
-std::string
-FogAuthenticator::getUserEdition() const
-{
-    return "community";
-}
